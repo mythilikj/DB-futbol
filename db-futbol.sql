de4de86da78d89123648c9f20157e4c336c6dc9f
JDSimbaqueba <102933031+JDSimbaqueba@users.noreply.github.com>
2023-05-04 19:42:24 -0500

    Update db-futbol.sql

--- db-futbol.sql
+++ db-futbol.sql
@@ -6,3 +6,8 @@ create table arbitro(
 	constraint arbitro_pk primary key(id)
 );
 
+create table equipo
+( id_equipo varchar(30), nombre varchar(50), valor_mill numeric(8,2),
+ constraint equipo_pk primary key(id_equipo)
+);
+
