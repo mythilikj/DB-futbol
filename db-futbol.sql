38ff550493936f43c3211d848e0b922891c17bd1
mythilikj <mythili.kasibhatla@urosario.edu.co>
2023-05-04 21:44:13 -0500

    Update db-futbol.sql

--- db-futbol.sql
+++ db-futbol.sql
@@ -6,8 +6,10 @@ create table arbitro(
 	constraint arbitro_pk primary key(id)
 );
 
-create table equipo
-( id_equipo varchar(30), nombre varchar(50), valor_mill numeric(8,2),
- constraint equipo_pk primary key(id_equipo)
+create table equipo(
+	id_equipo varchar(30), 
+	nombre varchar(50), 
+	valor_mill numeric(8,2),
+	constraint equipo_pk primary key(id_equipo)
 );
 
