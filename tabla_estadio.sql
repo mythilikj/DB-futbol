bcc9a8e94756f5f1db308cbe1e03a60f70e0fff9
mythilikj <102186528+mythilikj@users.noreply.github.com>
2023-05-04 21:57:52 -0500

    Update tabla_estadio.sql

--- tabla_estadio.sql
+++ tabla_estadio.sql
@@ -1,9 +1,11 @@
-create table estadio
-(codigo_estadio varchar(10) unique, nombre varchar(40)
- , capacidad integer, ciudad varchar(30),
- id_equipo_equipo varchar(10),
- constraint estadio_pk primary key(codigo_estadio),
- foreign key (id_equipo_equipo) references equipo
+create table estadio(
+	codigo_estadio varchar(10) unique, 
+	nombre varchar(40), 
+	capacidad integer, 
+	ciudad varchar(30), 
+	id_equipo_equipo varchar(10), 
+	constraint estadio_pk primary key(codigo_estadio), 
+	foreign key (id_equipo_equipo) references equipo
 );
 
 insert into estadio values ('1010', 'Etihad Stadium', 53400, 'Manchester', 10 );
