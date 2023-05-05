a75340162f26de4cf6c4791d3520860e94defd93
JDSimbaqueba <102933031+JDSimbaqueba@users.noreply.github.com>
2023-05-04 19:46:22 -0500

    Add files via upload

---
+++ tabla_equipo.sql
@@ -0,0 +1,28 @@
+create table equipo
+( id_equipo varchar(30), nombre varchar(50), valor_mill numeric(8,2),
+ constraint equipo_pk primary key(id_equipo));
+
+
+
+insert into equipo values('10', 'Manchester City', 1000.05 );
+insert into equipo values('11', 'Arsenal FC', 890.00 );
+insert into equipo values('12', 'Chelsea FC', 1000.02 );
+insert into equipo values('13', 'Liverpool FC', 879.00 );
+insert into equipo values('14', 'Tottenham Hotspur', 680.30 );
+
+
+insert into equipo values('15', 'Manchester City', 1000.05 );
+insert into equipo values('16', 'Manchester City', 1000.05 );
+insert into equipo values('17', 'Manchester City', 1000.05 );
+insert into equipo values('18', 'Manchester City', 1000.05 );
+insert into equipo values('19', 'Manchester City', 1000.05 );
+insert into equipo values('20', 'Manchester City', 1000.05 );
+insert into equipo values('21', 'Manchester City', 1000.05 );
+insert into equipo values('22', 'Manchester City', 1000.05 );
+insert into equipo values('23', 'Manchester City', 1000.05 );
+insert into equipo values('24', 'Manchester City', 1000.05 );
+insert into equipo values('25', 'Manchester City', 1000.05 );
+insert into equipo values('26', 'Manchester City', 1000.05 );
+insert into equipo values('27', 'Manchester City', 1000.05 );
+insert into equipo values('28', 'Manchester City', 1000.05 );
+insert into equipo values('29', 'Manchester City', 1000.05 );
