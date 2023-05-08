d6aeaf89ba7159b569bb02c3381565a21e16984f
JDSimbaqueba <102933031+JDSimbaqueba@users.noreply.github.com>
2023-05-08 11:57:36 -0500

    Create tabla_partido.sql

---
+++ tabla_partido.sql
@@ -0,0 +1,23 @@
+insert into partido values('0301', '15' , '20', 1 , 0 , '104', 1 );
+insert into partido values('0302', '27' , '19', 3 , 1 , '107', 1 );
+insert into partido values('0303', '23' , '24', 1 , 1 , '115', 0 );
+insert into partido values('0304', '28' , '25', 3 , 2 , '110', 1 );
+insert into partido values('0305', '18' , '21', 1 , 2 , '114', 2 );
+insert into partido values('0306', '29' , '11', 0 , 3 , '108', 2 );
+insert into partido values('0307', '26' , '12', 3 , 0 , '106', 1 );
+insert into partido values('0308', '17' , '22', 0 , 2 , '103', 2 );
+insert into partido values('0309', '16' , '10', 3 , 3 , '112', 0 );
+insert into partido values('0310', '14' , '13', 2 , 1 , '102', 1 );
+
+
+
+insert into partido values('0401', '21' , '14', 0 , 1 , '107', 2 );
+insert into partido values('0402', '25' , '23', 1 , 1 , '113', 0 );
+insert into partido values('0403', '22' , '26', 1 , 0 , '114', 1 );
+insert into partido values('0404', '12' , '18', 2 , 1 , '101', 1 );
+insert into partido values('0405', '13' , '29', 9 , 0 , '106', 1 );
+insert into partido values('0406', '10' , '27', 4 , 2 , '111', 1 );
+insert into partido values('0407', '11' , '29', 2 , 1 , '112', 1 );
+insert into partido values('0408', '19' , '17', 0 , 1 , '109', 2 );
+insert into partido values('0409', '20' , '16', 1 , 1 , '110', 0 );
+insert into partido values('0410', '24' , '15', 0 , 2 , '108', 2 );
